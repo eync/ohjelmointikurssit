6696b89d12b996fef275bd6b1da326c6623a5f8d
eync <1291215+eync@users.noreply.github.com>
2021-08-19 15:03:34 +0300

    small changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,12 +107,6 @@ module.exports = {
           editUrl:
             'https://github.com/eync/ohjelmointikurssit/',
         },
-        blog: {
-          // showReadingTime: true,
-          // // Please change this to your repo.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
